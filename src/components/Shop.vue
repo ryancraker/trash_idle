4fa60d3c812078592cb27fe7ad4f4ee7caa79157
ryancraker <ryan.craker98@gmail.com>
2025-08-30 18:24:39 -0600

    money for trash

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,32 +1,51 @@
 <script setup>
-import { AppState } from "@/AppState";
-import { computed, ref } from "vue";
+  import { AppState } from "@/AppState";
+  import { computed, ref } from "vue";
 
-// defineProps({shopItem})
+  // defineProps({shopItem})
 
-const store = ref({
-  pokeball: {
-    name: "Poké Ball",
-    price: 5,
-    inv: 0,
-    id: 1,
-  },
-  potion: {
-    name: "Potion",
-    price: 10,
-    inv: 0,
-    id: 2,
-  },
-  revive: {
-    name: "Revive",
-    price: 100,
-    inv: 0,
-    id: 3,
-  },
-});
+  // const store = ref({
+  //   pokeball: {
+  //     name: "Poké Ball",
+  //     price: 5,
+  //     inv: 0,
+  //     id: 1,
+  //   },
+  //   potion: {
+  //     name: "Potion",
+  //     price: 10,
+  //     inv: 0,
+  //     id: 2,
+  //   },
+  // revive: {
+  //   name: "Revive",
+  //   price: 100,
+  //   inv: 0,
+  //   id: 3,
+  // },
+  // });
 
-const money = computed(() => AppState.money);
-const is_visible = computed(() => AppState.is_shop_visible);
+  const store = [
+    {
+      price: 5,
+      inv: 0,
+      name: "Poké Ball"
+    },
+    {
+      name: "Potion",
+      price: 10,
+      inv: 0,
+    },
+    {
+      name: "Revive",
+      price: 100,
+      inv: 0,
+    },
+  ]
+  const money = computed(() => AppState.money);
+  const is_visible = computed(() => AppState.is_shop_visible);
+
+
 </script>
 
 <template>
@@ -36,11 +55,7 @@ const is_visible = computed(() => AppState.is_shop_visible);
       <h2>Store</h2>
       <section class="container">
         <div class="row">
-          <img
-            src="../assets/img/nurse-joy.png"
-            class="nurse-img"
-            alt="Nurse"
-          />
+          <img src="../assets/img/nurse-joy.png" class="nurse-img" alt="Nurse" />
         </div>
         <div class="row">${{ money }}</div>
         <div class="row">
@@ -57,10 +72,11 @@ const is_visible = computed(() => AppState.is_shop_visible);
 </template>
 
 <style lang="scss" scoped>
-#shop-window {
-  visibility: hidden;
-}
-#shop-window.active {
-  visibility: visible;
-}
+  #shop-window {
+    visibility: hidden;
+  }
+
+  #shop-window.active {
+    visibility: visible;
+  }
 </style>
--- src/pages/KiraPage.vue
+++ src/pages/KiraPage.vue
@@ -1,59 +1,67 @@
 <script setup>
-import { AppState } from "@/AppState";
-import RandomEncounter from "@/components/RandomEncounter.vue";
-import Shop from "@/components/Shop.vue";
-import { ref } from "vue";
+  import { AppState } from "@/AppState";
+  import RandomEncounter from "@/components/RandomEncounter.vue";
+  import Shop from "@/components/Shop.vue";
+  import { computed, ref } from "vue";
 
-const trash = ref(0);
-const health = ref(100);
-const level = ref({
-  now: 1,
-  max: 100,
-});
-const xp = ref({
-  now: 0,
-  max: 5,
-});
-const forage_skill = ref(1);
+  const trash = ref(0);
+  const health = ref(100);
+  const trash_exchange_rate = ref(1)
+  const money = computed(() => AppState.money)
+  const level = ref({
+    now: 1,
+    max: 100,
+  });
+  const xp = ref({
+    now: 0,
+    max: 5,
+  });
+  const forage_skill = ref(1);
 
-// @click="showMobileMenu = !showMobileMenu"
+  // @click="showMobileMenu = !showMobileMenu"
 
-function toggleStore() {
-  AppState.is_shop_visible = !AppState.is_shop_visible;
-}
-
-function toggleEncounter() {
-  AppState.is_encounter_visible = !AppState.is_encounter_visible;
-}
-
-function forage() {
-  trash.value += forage_skill.value;
-
-  /** XP & Leveling up */
-  xp.value.now++;
-  if (xp.value.now == xp.value.max) {
-    xp.value.now = 0;
-    xp.value.max *= 3;
-    level.value.now++;
-    forage_skill.value += 5;
+  function toggleStore() {
+    AppState.is_shop_visible = !AppState.is_shop_visible;
   }
 
-  /** Random encounter */
-  if (Math.ceil(Math.random() * 6) == 3) {
-    toggleEncounter();
+  function toggleEncounter() {
+    AppState.is_encounter_visible = !AppState.is_encounter_visible;
   }
-}
+
+  function forage() {
+    trash.value += forage_skill.value;
+
+    /** XP & Leveling up */
+    xp.value.now++;
+    if (xp.value.now == xp.value.max) {
+      xp.value.now = 0;
+      xp.value.max *= 3;
+      level.value.now++;
+      forage_skill.value += 5;
+    }
+
+    /** Random encounter */
+    if (Math.ceil(Math.random() * 6) == 3) {
+      toggleEncounter();
+    }
+  }
+  function trashForDollars() {
+    if (trash.value >= trash_exchange_rate.value) {
+      /** Adds one dollar */
+      AppState.money++
+      /** Subtracts trash per current exchange rate */
+      trash.value -= trash_exchange_rate.value
+      /** Increases current exchange rate by a factor of 3 */
+      trash_exchange_rate.value = Math.ceil(trash_exchange_rate.value *= 1.1)
+    }
+  }
 </script>
 
 <template>
   <div class="home-container">
     <article class="card shadow">
       <h2>Team Rocket's Hideout</h2>
-      <img
-        src="../assets/img/rocket-grunt.webp"
-        class="grunt-img"
-        alt="Rocket grunt"
-      />
+      <img src="../assets/img/rocket-grunt.webp" class="grunt-img" alt="Rocket grunt" />
     </article>
     <!-- MAIN WINDOW -->
     <article class="card shadow">
@@ -76,6 +84,9 @@ function forage() {
           </div>
           <div class="row">
             <span class="mdi mdi-trash-can-outline fs-5">{{ trash }}</span>
+            <span>${{ money }} <button class="btn btn-dark-cyan text-white" @click="trashForDollars()">Exchange Trash
+                for
+                $1</button></span>
           </div>
           <div class="row">
             <button class="btn btn-dark-cyan w-100" @click="forage()">
@@ -87,10 +98,7 @@ function forage() {
               <button class="btn btn-vue w-100">Pokémon Center</button>
             </div>
             <div class="col-4">
-              <button
-                class="btn btn-vue w-100 mdi mdi-store"
-                @click="toggleStore()"
-              >
+              <button class="btn btn-vue w-100 mdi mdi-store" @click="toggleStore()">
                 Store
               </button>
             </div>
@@ -109,42 +117,51 @@ function forage() {
 </template>
 
 <style scoped lang="scss">
-.home-container {
-  flex-grow: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-// Card
-article {
-  min-width: 400px;
-}
-img {
-  width: auto;
-  margin: auto;
-}
-img.grunt-img {
-  width: 100px;
-}
-h1,
-h2 {
-  text-align: center;
-}
-.card-body .row {
-  padding: 5px 0px;
-}
-.card-body .col-4 {
-  padding: 0px 3px;
-}
-.health-bar {
-  accent-color: hotpink;
-}
-#xp-bar {
-  accent-color: var(--bs-dark-cyan);
-}
-#xp-ratio {
-  text-align: right;
-  font-size: 11px;
-  padding: 0;
-}
+  .home-container {
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  // Card
+  article {
+    min-width: 400px;
+  }
+
+  img {
+    width: auto;
+    margin: auto;
+  }
+
+  img.grunt-img {
+    width: 100px;
+  }
+
+  h1,
+  h2 {
+    text-align: center;
+  }
+
+  .card-body .row {
+    padding: 5px 0px;
+  }
+
+  .card-body .col-4 {
+    padding: 0px 3px;
+  }
+
+  .health-bar {
+    accent-color: hotpink;
+  }
+
+  #xp-bar {
+    accent-color: var(--bs-dark-cyan);
+  }
+
+  #xp-ratio {
+    text-align: right;
+    font-size: 11px;
+    padding: 0;
+  }
 </style>
